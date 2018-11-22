5d30b35a81cd41f438040ad417bbd990e653f210
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-11-23 03:05:13 +0600

    3 styles

--- src/Components/Home/features/index.jsx
+++ src/Components/Home/features/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Stripe from './stripe'
+import Text from './text'
 
 export default function Home() {
   return (
     <div class="featured_wrapper">
         <Stripe/>
+        <Text/>
       
     </div>
   )
--- src/Components/Home/features/stripe.jsx
+++ src/Components/Home/features/stripe.jsx
@@ -67,7 +67,6 @@ class Stripe extends Component {
 
            }}
             >
-   
             </div>
            )
         }}
