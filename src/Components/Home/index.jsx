773fc0696181848e9a923965bae53721a117754e
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-11-23 23:51:21 +0600

    ready for works matches

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Features from '../Home/features/index'
+import Matches from '../matches/index'
 
 export default function index() {
   return (
     <div class="bck_blue">
         <Features/>
+        
+        <Matches/>
       
     </div>
   )
---
+++ src/Components/matches/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+ const  HomeMatch = () => {
+  return (
+    <div className="home_matches_wrapper">
+       <div className="container">
+         TAG
+         HOME
+         TAG
+       </div>
+      
+    </div>
+  )
+}
+
+export default HomeMatch
+
+
