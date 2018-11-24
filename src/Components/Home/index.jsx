6d8221c4e514f56c0e481c1111873de5f7326f6c
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-11-25 01:04:10 +0600

    made text the team

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Features from '../Home/features/index'
 import Matches from '../Home/matches/index'
+import MeetPlayer from '../Home/meetPlayer/index'
 
 export default function index() {
   return (
     <div class="bck_blue">
         <Features/>
-        
         <Matches/>
+        <MeetPlayer/>
       
     </div>
   )
