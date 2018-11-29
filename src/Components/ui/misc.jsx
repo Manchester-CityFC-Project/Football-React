d1b35bb5a659854ae2520a27bc766033fe7dcfa5
mdmirajulkarim <mdmirajulkarim@gmail.com>
2018-11-29 15:14:03 +0600

    cool way validation test

--- src/Components/ui/misc.jsx
+++ src/Components/ui/misc.jsx
@@ -29,6 +29,12 @@ export const Tag = (props) => {
 
 export const validate =(element) => {
     let error = [true,''];
+    if(element.validation.email) {
+         const valid = /\S+@\S+\.\S+/.test(element.value);
+         const message = `${!valid ? 'Must be a valid email':''}`;
+         error = !valid ? [valid,message]:error    
+    }
+
 
     if(element.validation.required)
     {
